540512c5afd92aaf7a29266de009aab204ac10a9
Owen Conoly <otconoly@hotmail.com>
2025-12-15 17:20:00 -0500

    add invert tactic (stolen from frap)

--- src/Tactics.v
+++ src/Tactics.v
@@ -11,3 +11,21 @@ Ltac epose_dep H :=
   | ?A -> ?B => fail
   | forall _, _ => epose proof (H _) as H
   end.
+
+Ltac invert' H := inversion H; clear H; subst.
+
+Ltac invertN n :=
+  match goal with
+    | [ |- forall x : ?E, _ ] =>
+      match type of E with
+        | Prop =>
+          let H := fresh in intro H;
+            match n with
+              | 1 => invert' H
+              | S ?n' => invertN n'
+            end
+        | _ => intro; invertN n
+      end
+  end.
+
+Ltac invert e := invertN e || invert' e.
