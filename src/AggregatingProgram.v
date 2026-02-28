2d4154b640aa07902de40cce6eee2417c676c728
Owen Conoly <otconoly@hotmail.com>
2026-02-28 17:21:04 -0500

    more

--- src/AggregatingProgram.v
+++ src/AggregatingProgram.v
@@ -3,7 +3,7 @@ From Stdlib Require Import Arith.EqNat.
 From Stdlib Require Import Bool.Bool.
 From Stdlib Require Import Lists.List.
 From Stdlib Require Import micromega.Lia.
-From coqutil Require Import Map.Interface Map.Properties Map.Solver.
+From coqutil Require Import Map.Interface Map.Properties Map.Solver Datatypes.List.
 From Datalog Require Import List Datalog (* FancyNotations *) .
 Import ListNotations.
 
@@ -11,15 +11,23 @@ Section __.
 Variant bop := sum | prod.
 Variant type := val | set.
 Let rel : Type := nat.
+Variant obj0 :=
+  | natobj' (_ : nat).
 Variant obj :=
-  | natobj (_ : nat)
+  | primitive (_ : obj0)
+  (* | setobj (_ : nat -> Prop) *)
+  (* | listsetobj (_ : list nat -> Prop) *)
   | setobj (_ : nat -> Prop)
-  | listsetobj (_ : list nat -> Prop)
-  | blank.
-
+  | factset (_ : rel * list obj0 -> Prop)
+  | blank
+  | iter.
+Context (context : map.map nat obj).
+Definition natobj x := primitive (natobj' x).
+Let fact : Type := fact rel obj.
 Variant fn :=
   | fn_lit (o : obj)
-  | fn_bop (o : bop).
+  | fn_bop (o : bop)
+  | fn_fun (f : list (rel * list obj0 -> Prop) -> rel * list obj0 -> Prop).
 Let rule := rule rel nat fn bop.
 Let expr := expr nat fn.
 
@@ -43,6 +51,77 @@ Arguments Sexpr : clear implicits.
 
 Definition lit x : expr := fun_expr (fn_lit x) [].
 
+Definition union : list expr -> expr :=
+  fun_expr (fn_fun (fun args x => Exists (fun P => P x) args)).
+
+Print signature.
+
+Definition interp_bop o x y :=
+  match o with
+  | sum => x + y
+  | prod => x * y
+  end.
+Print fn. Print option_all.
+Definition interp_fn (f : fn) (args : list obj) : option obj :=
+  match f with
+  | fn_lit x => Some x
+  | fn_bop o =>
+      match args with
+      | [primitive (natobj' x); primitive (natobj' y)] => Some (natobj (interp_bop o x y))
+      | _ => None
+      end
+  | fn_fun g =>
+      match option_all (map (fun x =>
+                               match x with
+                               | factset f => Some f
+                               | _ => None
+                               end) args)
+      with
+      | Some args' => Some (factset (g args'))
+      | None => None
+      end
+  end.
+Print signature. Print obj.
+Definition get_set (s : obj) :=
+  match s with
+  | primitive _ => None
+  | setobj P => Some (fun x =>
+                       match x with
+                       | primitive (natobj' x0) => P x0
+                       | _ => False
+                       end)
+  | factset fs => None
+  | blank => None
+  | iter => None
+  end.
+
+Check fold_right.
+Definition extract_nat (x : obj) :=
+  match x with
+  | primitive (natobj' x0) => Some x0
+  | _ => None
+  end.
+
+Definition interp_agg o (i_xis : list (obj * obj)) :=
+  match option_all (map extract_nat (map snd i_xis)) with
+  | Some xis => natobj (fold_right (interp_bop o) (bop_id o) xis)
+  | None => natobj O
+  end.
+
+Instance Sig : signature fn bop obj :=
+  { interp_fun := interp_fn ;
+    get_set := get_set;
+    blank := blank;
+    interp_agg := interp_agg }.
+
+Definition closure (p : list rule) : list expr -> expr :=
+  fun_expr (fn_fun (fun args x => prog_impl_implication (sig := Sig) p
+                                 (fun y => exists y',
+                                      Exists (fun P => P y') args /\
+                                        y = {| fact_R := (fst y', normal);
+                                              fact_args := map primitive (snd y') |})
+                                 {| fact_R := (fst x, normal); fact_args := map primitive (snd x) |})).
+
 Definition bare_rule : Type := (rel * list expr) * list (rel * list expr).
 
 Definition is_blank (e : expr) :=
@@ -54,7 +133,7 @@ Definition is_blank (e : expr) :=
 (* Definition meta_rule_of (p : list rule) (r : bare_rule) := *)
 (*   let (concl, hyps) := r in *)
 
-Fixpoint compile_Sexpr (name : nat) (out : rel) {t} (e : Sexpr (fun _ => nat) t) : nat * list rule * :=
+Fixpoint compile_Sexpr (name : nat) (out : rel) {t} (e : Sexpr (fun _ => nat) t) : nat * list rule :=
   match e with
   | Var t x => (name,
                 [normal_rule
@@ -74,6 +153,10 @@ Fixpoint compile_Sexpr (name : nat) (out : rel) {t} (e : Sexpr (fun _ => nat) t)
           [{| clause_R := (out, normal); clause_args := [fun_expr (fn_bop o) [var_expr O; var_expr (S O)]] |}]
           [{| clause_R := (x', normal); clause_args := [var_expr O] |};
            {| clause_R := (y', normal); clause_args := [var_expr (S O)] |}]
+          :: normal_rule
+          [{| clause_R := (out, meta); clause_args := [union; lit blank]|}]
+          [{| clause_R := (x', meta); clause_args := [var_expr O; lit blank]|};
+           {| clause_R := (y', meta); clause_args := [var_expr (S O); lit blank]|}]
           :: p1 ++ p2)
   | empty => (name, [normal_rule
                       [{| clause_R := (out, meta); clause_args := [fun_expr (fn_lit (listsetobj (fun _ => False))) []; fun_expr (fn_lit blank) []] |}]
@@ -106,12 +189,6 @@ Definition interp_type t : Type :=
   match t with
   | val => nat
   | set => nat -> Prop
-  end.
-
-Definition interp_bop o x y :=
-  match o with
-  | sum => x + y
-  | prod => x * y
   end.
 
 Inductive interp_Sexpr : forall {t}, Sexpr interp_type t -> interp_type t -> Prop :=
