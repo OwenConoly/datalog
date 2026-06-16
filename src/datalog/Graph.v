647bfbfc5c74bf2f91147ad20b31aab8cc833b88
Owen Conoly <otconoly@hotmail.com>
2026-06-16 15:01:31 -0400

    finish grpah semantics, probably

--- src/datalog/Graph.v
+++ src/datalog/Graph.v
@@ -18,38 +18,35 @@ Section __.
       | I_event (_ : message)
       | O_event (_ : list message).
 
-    Context (node_step : node_prog -> node_state -> list IO_event -> node_state -> Prop).
+    Context (node_step : node_prog -> node_state -> IO_event -> node_state -> Prop).
 
     Record graph_state :=
       { g_nodes : node_states;
         g_messages : list (node_id (*destination*) * message) }.
 
-    Inductive graph_step : graph_state -> option IO_event -> graph_state -> Prop :=
+    Inductive graph_step : graph_state -> IO_event -> graph_state -> Prop :=
     | gstep_input gs n m :
       input_allowed n m = true ->
-      graph_step gs
-        [I_event m]
-        {| g_nodes := gs.(g_nodes);
-          g_messages := (n, m) :: gs.(g_messages) |}
+      graph_step gs (I_event m)
+                 {| g_nodes := gs.(g_nodes);
+                   g_messages := (n, m) :: gs.(g_messages) |}
     | gstep_run gs n np ns ns' outs :
       map.get p n = Some np ->
       map.get gs.(g_nodes) n = Some ns ->
-      node_step np ns [O_event outs] ns' ->
-      graph_step gs
-        [O_event (filter (output_visible n) outs)]
-        {| g_nodes := map.put gs.(g_nodes) n ns';
-          g_messages := gs.(g_messages) ++
-                             flat_map (fun m => map (fun n' => (n', m)) (forward n m))
-                             outs |}
+      node_step np ns (O_event outs) ns' ->
+      graph_step gs (O_event (filter (output_visible n) outs))
+                 {| g_nodes := map.put gs.(g_nodes) n ns';
+                   g_messages := gs.(g_messages) ++
+                                      flat_map (fun m => map (fun n' => (n', m)) (forward n m))
+                                      outs |}
     | gstep_receive gs n np ns ns' m ms1 ms2 :
       map.get p n = Some np ->
       map.get gs.(g_nodes) n = Some ns ->
-      node_step np ns [I_event m] ns' ->
+      node_step np ns (I_event m) ns' ->
       gs.(g_messages) = ms1 ++ (n, m) :: ms2 ->
-      graph_step gs
-        []
-        {| g_nodes := map.put gs.(g_nodes) n ns';
-          g_messages := ms1 ++ ms2 |}.
+      graph_step gs (O_event [])
+                 {| g_nodes := map.put gs.(g_nodes) n ns';
+                   g_messages := ms1 ++ ms2 |}.
   End graph.
 
   Section graphs.
@@ -60,3 +57,5 @@ Section __.
     Context {node_prog2 : Type} {graph_prog2 : map.map node_id node_prog2}.
     Context {node_state2 : Type} {node_states2 : map.map node_id node_state2}.
     Context (p2 : graph_prog2).
+  End graphs.
+End __.
