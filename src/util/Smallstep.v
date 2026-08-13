cf2256a05184414df8f66e08ac5d636569316e09
Owen Conoly <otconoly@hotmail.com>
2026-08-13 12:53:45 -0400

    try again with label_sim defn

--- src/util/Smallstep.v
+++ src/util/Smallstep.v
@@ -963,14 +963,14 @@ Section steps_corresp.
           R s1' (e :: t1) s2' (t2' ++ t2).
 
     Definition label_sim :=
-      forall s1 t1 t2 s1' s2 lbl1 outs,
+      forall s1 t1 t2 s2,
         R s1 t1 s2 t2 ->
-        step1 s1 (O_event lbl1 outs) s1' ->
-        exists s2' t2,
-          star step2 s2 t2 s2' /\
-            forall s1'' t1',
-              star step1 s1 t1' s1'' ->
-              In lbl1 (flat_map labels_of t1').
+        forall l1, exists l2s,
+        forall s1' t1' s2' t2',
+          star step2 s2 t2' s2' ->
+          star step1 s1 t1' s1' ->
+          incl l2s (flat_map labels_of t2') ->
+          In l1 (flat_map labels_of t1').
 
     Lemma weak_sim_lift :
       weak_sim ->
