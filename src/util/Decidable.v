9f9b2bdac04bcfde704cf7415483919cb7e6a42b
Owen Conoly <otconoly@hotmail.com>
2026-09-14 22:47:52 -0400

    a nice solution to the extensionality problem

--- src/util/Decidable.v
+++ src/util/Decidable.v
@@ -5,3 +5,11 @@ Lemma Reflects_iff P Q b :
   P <-> Q ->
   Reflects Q b.
 Proof. intros HP Hiff. destruct HP; constructor; tauto. Qed.
+
+Lemma Reflects_true_iff P b {H : Reflects P b} :
+  b = true <-> P.
+Proof. destruct H; intuition congruence. Qed.
+
+Lemma Reflects_false_iff P b `{H : Reflects P b} :
+  b = false <-> ~P.
+Proof. destruct H; intuition congruence. Qed.
