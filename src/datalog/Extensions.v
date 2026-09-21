e29230d99b01313f72c485203c8ac367396086c6
Owen Conoly <otconoly@hotmail.com>
2026-09-20 23:16:12 -0400

    "extensions"

---
+++ src/datalog/Extensions.v
@@ -0,0 +1,14 @@
+From Datalog Require Import Datalog Blocks List Pftree.
+From Stdlib Require Import List.
+
+Section __.
+  Context `{params : datalog_params}.
+
+  (*add some arbitrary extensions Ps?  local because not used now*)
+  #[local] Definition interp_step_with_extensions Ps p c hyps :=
+    program.interp_step p c hyps \/ Exists (fun P => P c hyps) Ps.
+End __.
+
+Module fprog.
+  Section __.
+    Definition
